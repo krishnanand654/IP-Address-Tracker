d76daf51b24dfe3814d7258a0bebef3358fb86e6
krishnanand654 <krishnanand654@gmail.com>
2022-06-08 19:41:04 +0530

    😬

--- app.js
+++ app.js
@@ -1,90 +1,69 @@
-
-function cipher(str1)
- {
-	var hex  = str1.toString();
-	var str = '';
-	for (var n = 0; n < hex.length; n += 2) {
-		str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
-	}
-	return str;
- }
-
-
 const getdata = () => {
-  
-  
+  var x = document.getElementById("myip").value;
 
-  let token_map = cipher('706b2e65794a31496a6f6961334a706332687559573568626d51694c434a68496a6f69593277795a324e7163574a6a4d444a7362444e6a623246354e7a563463444a3265694a392e75615f334f7a6757554a6d30654b5079676236394a51');
+  console.log(x);
 
- 
-    var x = document.getElementById("myip").value;
-   
-
-    console.log(x);
-    
-    fetch("http://ip-api.com/json/"+x+"?fields=status,message,country,countryCode,region,regionName,city,zip,lat,lon,timezone,offset,isp,org,as,query")
-    .then( (response) => {
-   
+  fetch(
+    "http://ip-api.com/json/" +
+      x +
+      "?fields=status,message,country,countryCode,region,regionName,city,zip,lat,lon,timezone,offset,isp,org,as,query"
+  )
+    .then((response) => {
       return response.json();
-    
     })
-    .then( (responseData) => {
+    .then((responseData) => {
       console.log(responseData);
 
       console.log(responseData.offset);
 
       d = Number(responseData.offset);
       var h = Math.floor(d / 3600);
-      var m = Math.floor(d % 3600 / 60);
-      var s = Math.floor(d % 3600 % 60);
+      var m = Math.floor((d % 3600) / 60);
+      var s = Math.floor((d % 3600) % 60);
 
+      document.querySelector("#ip").innerHTML = responseData.query;
+      document.querySelector("#loc").innerHTML = responseData.city;
+      document.querySelector("#tme").innerHTML = h + ":" + m;
+      document.querySelector("#isp").innerHTML = responseData.isp;
 
-      document.querySelector('#ip').innerHTML =  responseData.query;
-      document.querySelector('#loc').innerHTML =  responseData.city;
-      document.querySelector('#tme').innerHTML =  "-"+ h +':'+ m;
-      document.querySelector('#isp').innerHTML =  responseData.isp;
+      var map = L.map("map").setView([responseData.lat, responseData.lon], 13);
 
-      
-      var map = L.map('map').setView([responseData.lat, responseData.lon], 13);
-
-      var container = L.DomUtil.get('map');
-      if(container != null){
-      container._leaflet_id = null;
+      var container = L.DomUtil.get("map");
+      if (container != null) {
+        container._leaflet_id = null;
       }
-      
-        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token='+token_map, {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: token_map
-      }).addTo(map);
 
+      L.tileLayer(
+        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=" +
+          "pk.eyJ1Ijoia3Jpc2huYW5hbmQiLCJhIjoiY2wyZ2NqcWJjMDJsbDNjb2F5NzV4cDJ2eiJ9.ua_3OzgWUJm0eKPygb69JQ",
+        {
+          attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          maxZoom: 18,
+          id: "mapbox/streets-v11",
+          tileSize: 512,
+          zoomOffset: -1,
+          accessToken:
+            "pk.eyJ1Ijoia3Jpc2huYW5hbmQiLCJhIjoiY2wyZ2NqcWJjMDJsbDNjb2F5NzV4cDJ2eiJ9.ua_3OzgWUJm0eKPygb69JQ",
+        }
+      ).addTo(map);
 
       var greenIcon = new L.Icon({
-        iconUrl: './images/icon-location.svg',
+        iconUrl: "./images/icon-location.svg",
         iconSize: [40, 50],
         iconAnchor: [12, 41],
         popupAnchor: [1, -34],
-        shadowSize: [41, 41]
+        shadowSize: [41, 41],
       });
-      map.invalidateSize()
+      map.invalidateSize();
 
-
-      var marker = L.marker([responseData.lat, responseData.lon], {icon: greenIcon}).addTo(map);
-     
-    
-
-
-    }) 
-    .catch(error => {
-      console.log(error);
+      var marker = L.marker([responseData.lat, responseData.lon], {
+        icon: greenIcon,
+      }).addTo(map);
     })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
-  
-
-  
-  }
-
-  getdata();
+getdata();
